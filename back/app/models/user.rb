4d6f2a70835c780d820ecc0420600cce9ac913fc
Maria Clara Barroso <maria.barroso@midr.gov.br>
2023-11-21 17:44:47 -0300

    Create front and back

---
+++ back/app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :name, :email, :phone, :is_admin, presence: true
+end
